459f0ae20a292796d39ef9a64423c034310c0c83
adrianlukasik <adrian.lukasik@technischools.com>
2026-09-21 11:19:06 +0200

    Added credentials

--- main.py
+++ main.py
@@ -12,23 +12,34 @@ cur.execute("""CREATE TABLE IF NOT EXISTS users
                    phone   VARCHAR(12) NOT NULL
                )""")
 
-cur.execute("""CREATE TABLE IF NOT EXISTS transactions
-               (
-                   id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                   amount      DECIMAL(10, 2) NOT NULL,
-                   userfrom_id INTEGER        NOT NULL,
-                   userto_id   INTEGER        NOT NULL,
-                   date        VARCHAR(20)    NOT NULL,
-                   FOREIGN KEY (userfrom_id) REFERENCES users (id),
-                   FOREIGN KEY (userto_id) REFERENCES users (id),
-                   CHECK (userfrom_id != userto_id)
-               )
-            """)
 cur.execute("""CREATE TABLE IF NOT EXISTS accounts
                (
                    id      INTEGER PRIMARY KEY AUTOINCREMENT,
                    amount  DECIMAL(20, 2) NOT NULL,
                    user_id INTEGER        NOT NULL,
+                   FOREIGN KEY (user_id) REFERENCES users (id)
+               )
+            """)
+
+cur.execute("""CREATE TABLE IF NOT EXISTS transactions
+               (
+                   id          INTEGER PRIMARY KEY AUTOINCREMENT,
+                   amount      DECIMAL(10, 2) NOT NULL,
+                   account_from_id INTEGER        NOT NULL,
+                   account_to_id   INTEGER        NOT NULL,
+                   date        VARCHAR(20)    NOT NULL,
+                   FOREIGN KEY (account_from_id) REFERENCES accounts (id),
+                   FOREIGN KEY (account_to_id) REFERENCES accounts (id),
+                   CHECK (account_from_id != account_to_id)
+               )
+            """)
+
+cur.execute("""CREATE TABLE IF NOT EXISTS credentials
+               (
+                   id       INTEGER PRIMARY KEY AUTOINCREMENT,
+                   login    VARCHAR(50)  NOT NULL,
+                   password VARCHAR(150) NOT NULL,
+                   user_id  INTEGER      NOT NULL,
                    FOREIGN KEY (user_id) REFERENCES users (id)
                )
             """)
